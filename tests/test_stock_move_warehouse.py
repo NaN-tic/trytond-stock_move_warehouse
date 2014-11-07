0069a46f2d0c239a264e623e0d22241342785440
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:47:04 +0100

    Cleanup tests

--- tests/test_stock_move_warehouse.py
+++ tests/test_stock_move_warehouse.py
@@ -1,38 +1,24 @@
 #!/usr/bin/env python
-#This file is part stock_move_warehouse module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# This file is part stock_move_warehouse module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
 
 
 class StockMoveWarehouseTestCase(unittest.TestCase):
-    '''
-    Test Stock Move Warehouse module.
-    '''
+    'Test Stock Move Warehouse module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('stock_move_warehouse')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('stock_move_warehouse')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
